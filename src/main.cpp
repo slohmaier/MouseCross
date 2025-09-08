e33458fd06a475778c37355d3f97a14a729323b3
Stefan Lohmaier <stefan@slohmaier.de>
2025-09-08 11:23:05 +0200

    add german translation

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <QSharedMemory>
 #include <QLocalSocket>
 #include <QMessageBox>
+#include <QTranslator>
+#include <QLocale>
 #include "MouseCrossApp.h"
 
 int main(int argc, char *argv[])
@@ -14,6 +16,17 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationName("MouseCross");
     QCoreApplication::setApplicationName("MouseCross");
     QCoreApplication::setApplicationVersion("0.1.0");
+    
+    // Load translations
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "mousecross_" + QLocale(locale).name().section('_', 0, 0);
+        if (translator.load(":/translations/" + baseName)) {
+            app.installTranslator(&translator);
+            break;
+        }
+    }
     
     // Single instance check using QSharedMemory
     QSharedMemory sharedMemory("MouseCross-SingleInstance-Key");
---
+++ translations/mousecross_de.ts
@@ -0,0 +1,253 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../src/AboutDialog.cpp" line="16"/>
+        <source>About MouseCross</source>
+        <translation>Über MouseCross</translation>
+    </message>
+    <message>
+        <location filename="../src/AboutDialog.cpp" line="40"/>
+        <source>MouseCross</source>
+        <translation>MouseCross</translation>
+    </message>
+    <message>
+        <location filename="../src/AboutDialog.cpp" line="49"/>
+        <source>Version 0.1.0</source>
+        <translation>Version 0.1.0</translation>
+    </message>
+    <message>
+        <location filename="../src/AboutDialog.cpp" line="55"/>
+        <source>A crosshair overlay application designed to help visually impaired users with tunnel vision locate their mouse cursor on the screen.</source>
+        <translation>Eine Fadenkreuz-Overlay-Anwendung, die sehbehinderten Benutzern mit Tunnelblick dabei hilft, ihren Mauszeiger auf dem Bildschirm zu finden.</translation>
+    </message>
+    <message>
+        <location filename="../src/AboutDialog.cpp" line="66"/>
+        <source>© 2024 MouseCross
+Built with Qt %1
+
+https://slohmaier.de/MouseCross</source>
+        <translation>© 2024 MouseCross
+Erstellt mit Qt %1
+
+https://slohmaier.de/MouseCross</translation>
+    </message>
+    <message>
+        <location filename="../src/AboutDialog.cpp" line="80"/>
+        <source>Show Welcome</source>
+        <translation>Willkommen anzeigen</translation>
+    </message>
+    <message>
+        <location filename="../src/AboutDialog.cpp" line="83"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+</context>
+<context>
+    <name>MouseCrossApp</name>
+    <message>
+        <location filename="../src/MouseCrossApp.cpp" line="45"/>
+        <location filename="../src/MouseCrossApp.cpp" line="180"/>
+        <location filename="../src/MouseCrossApp.cpp" line="186"/>
+        <source>MouseCross</source>
+        <translation>MouseCross</translation>
+    </message>
+    <message>
+        <location filename="../src/MouseCrossApp.cpp" line="46"/>
+        <source>System tray is not available on this system</source>
+        <translation>Die Systemleiste ist auf diesem System nicht verfügbar</translation>
+    </message>
+    <message>
+        <location filename="../src/MouseCrossApp.cpp" line="71"/>
+        <source>&amp;Toggle Crosshair</source>
+        <translation>&amp;Fadenkreuz umschalten</translation>
+    </message>
+    <message>
+        <location filename="../src/MouseCrossApp.cpp" line="74"/>
+        <source>&amp;Settings...</source>
+        <translation>&amp;Einstellungen...</translation>
+    </message>
+    <message>
+        <location filename="../src/MouseCrossApp.cpp" line="77"/>
+        <source>&amp;About...</source>
+        <translation>&amp;Über...</translation>
+    </message>
+    <message>
+        <location filename="../src/MouseCrossApp.cpp" line="80"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Beenden</translation>
+    </message>
+    <message>
+        <location filename="../src/MouseCrossApp.cpp" line="126"/>
+        <source>MouseCross - Visual Mouse Locator</source>
+        <translation>MouseCross - Visueller Mauszeiger-Finder</translation>
+    </message>
+    <message>
+        <location filename="../src/MouseCrossApp.cpp" line="179"/>
+        <source>&amp;Hide Crosshair</source>
+        <translation>&amp;Fadenkreuz ausblenden</translation>
+    </message>
+    <message>
+        <location filename="../src/MouseCrossApp.cpp" line="181"/>
+        <source>Crosshair activated</source>
+        <translation>Fadenkreuz aktiviert</translation>
+    </message>
+    <message>
+        <location filename="../src/MouseCrossApp.cpp" line="185"/>
+        <source>&amp;Show Crosshair</source>
+        <translation>&amp;Fadenkreuz einblenden</translation>
+    </message>
+    <message>
+        <location filename="../src/MouseCrossApp.cpp" line="187"/>
+        <source>Crosshair deactivated</source>
+        <translation>Fadenkreuz deaktiviert</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../src/SettingsDialog.cpp" line="26"/>
+        <source>MouseCross Settings</source>
+        <translation>MouseCross-Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../src/SettingsDialog.cpp" line="49"/>
+        <source>Restore Defaults</source>
+        <translation>Standardwerte wiederherstellen</translation>
+    </message>
+    <message>
+        <location filename="../src/SettingsDialog.cpp" line="52"/>
+        <source>Close</source>
+        <translation>Schließen</translation>
+    </message>
+    <message>
+        <location filename="../src/SettingsDialog.cpp" line="65"/>
+        <source>Crosshair Appearance</source>
+        <translation>Fadenkreuz-Aussehen</translation>
+    </message>
+    <message>
+        <location filename="../src/SettingsDialog.cpp" line="71"/>
+        <source>Line Width:</source>
+        <translation>Linienbreite:</translation>
+    </message>
+    <message>
+        <location filename="../src/SettingsDialog.cpp" line="75"/>
+        <source>Base thickness (automatically scaled for high DPI displays)</source>
+        <translation>Grunddicke (automatisch für hochauflösende Bildschirme skaliert)</translation>
+    </message>
+    <message>
+        <location filename="../src/SettingsDialog.cpp" line="81"/>
+        <source>Offset from Cursor:</source>
+        <translation>Abstand zum Zeiger:</translation>
+    </message>
+    <message>
+        <location filename="../src/SettingsDialog.cpp" line="85"/>
+        <source>Distance from mouse cursor where lines start</source>
+        <translation>Entfernung vom Mauszeiger, wo die Linien beginnen</translation>
+    </message>
+    <message>
+        <location filename="../src/SettingsDialog.cpp" line="91"/>
+        <source>Thickness Growth:</source>
+        <translation>Dicken-Zunahme:</translation>
+    </message>
+    <message>
+        <location filename="../src/SettingsDialog.cpp" line="108"/>
+        <source>Opacity:</source>
+        <translation>Deckkraft:</translation>
+    </message>
+    <message>
+        <location filename="../src/SettingsDialog.cpp" line="124"/>
+        <source>Color:</source>
+        <translation>Farbe:</translation>
+    </message>
+    <message>
+        <location filename="../src/SettingsDialog.cpp" line="132"/>
+        <source>Show direction arrows on inner line</source>
+        <translation>Richtungspfeile auf innerer Linie anzeigen</translation>
+    </message>
+    <message>
+        <location filename="../src/SettingsDialog.cpp" line="139"/>
+        <source>Behavior</source>
+        <translation>Verhalten</translation>
+    </message>
+    <message>
+        <location filename="../src/SettingsDialog.cpp" line="142"/>
+        <source>Start with Windows/macOS</source>
+        <translation>Mit Windows/macOS starten</translation>
+    </message>
+    <message>
+        <location filename="../src/SettingsDialog.cpp" line="146"/>
+        <source>Activate crosshair on application start</source>
+        <translation>Fadenkreuz beim Programmstart aktivieren</translation>
+    </message>
+    <message>
+        <location filename="../src/SettingsDialog.cpp" line="153"/>
+        <source>Hotkey</source>
+        <translation>Tastenkürzel</translation>
+    </message>
+    <message>
+        <location filename="../src/SettingsDialog.cpp" line="156"/>
+        <source>Toggle hotkey:</source>
+        <translation>Umschalt-Tastenkürzel:</translation>
+    </message>
+    <message>
+        <location filename="../src/SettingsDialog.cpp" line="183"/>
+        <source>Select Crosshair Color</source>
+        <translation>Fadenkreuz-Farbe auswählen</translation>
+    </message>
+</context>
+<context>
+    <name>WelcomeDialog</name>
+    <message>
+        <location filename="../src/WelcomeDialog.cpp" line="16"/>
+        <location filename="../src/WelcomeDialog.cpp" line="28"/>
+        <source>Welcome to MouseCross</source>
+        <translation>Willkommen bei MouseCross</translation>
+    </message>
+    <message>
+        <location filename="../src/WelcomeDialog.cpp" line="38"/>
+        <source>MouseCross helps visually impaired users locate their mouse cursor by displaying a customizable crosshair overlay.</source>
+        <translation>MouseCross hilft sehbehinderten Benutzern dabei, ihren Mauszeiger zu finden, indem es ein anpassbares Fadenkreuz-Overlay anzeigt.</translation>
+    </message>
+    <message>
+        <location filename="../src/WelcomeDialog.cpp" line="46"/>
+        <source>Key features:
+• Always-on-top crosshair overlay
+• Customizable appearance and size
+• Inverted mode for visibility on any background
+• Configurable hotkey for quick toggle
+• Auto-start with Windows/macOS
+
+The application runs from the system tray. Double-click the tray icon to toggle the crosshair.
+
+More information: https://slohmaier.de/MouseCross</source>
+        <translation>Wichtige Funktionen:
+• Immer im Vordergrund liegendes Fadenkreuz-Overlay
+• Anpassbares Aussehen und Größe
+• Invertierter Modus für Sichtbarkeit auf jedem Hintergrund
+• Konfigurierbares Tastenkürzel zum schnellen Umschalten
+• Automatischer Start mit Windows/macOS
+
+Die Anwendung läuft über die Systemleiste. Doppelklicken Sie auf das Symbol in der Systemleiste, um das Fadenkreuz umzuschalten.
+
+Weitere Informationen: https://slohmaier.de/MouseCross</translation>
+    </message>
+    <message>
+        <location filename="../src/WelcomeDialog.cpp" line="63"/>
+        <source>Don&apos;t show this welcome screen again</source>
+        <translation>Diesen Willkommensbildschirm nicht mehr anzeigen</translation>
+    </message>
+    <message>
+        <location filename="../src/WelcomeDialog.cpp" line="70"/>
+        <source>Open Settings</source>
+        <translation>Einstellungen öffnen</translation>
+    </message>
+    <message>
+        <location filename="../src/WelcomeDialog.cpp" line="73"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+</context>
+</TS>
